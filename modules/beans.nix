de482c4ed84e7370d25970d179af4c8688b53731
Silvio Böhler <sboehler@noreply.users.github.com>
2018-10-01 20:31:23 +0200

    Upgrade beans

--- modules/beans.nix
+++ modules/beans.nix
@@ -1,16 +1,16 @@
 { mkDerivation, base, bifunctors, bytestring, containers
-, exceptions, fetchgit, filepath, groups, hpack, megaparsec, mtl
+, exceptions, fetchgit, filepath, groups, megaparsec, mtl
 , optparse-applicative, prettyprinter, regex-pcre, scientific
-, stdenv, tasty, tasty-hunit, tasty-quickcheck, tasty-smallcheck
-, text, time
+, stdenv, tasty, tasty-golden, tasty-hunit, tasty-quickcheck
+, tasty-smallcheck, text, time
 }:
 mkDerivation {
   pname = "beans";
   version = "0.1.0.0";
   src = fetchgit {
     url = "https://github.com/sboehler/beans";
-    sha256 = "0hfsia7rs104r238p2pghwnj38wll4fhlx3y3y0c9l155f32h601";
-    rev = "dd55bdf65268a3987884176fb7a24b35d6cd44cf";
+    sha256 = "1xjd3fq3iqfxfk2wx3qhdkl0hssxpcphpmvwysjv9zwlxnpsrxhc";
+    rev = "2b088e7325ec73058ac91277df0b5403fcbd2bff";
   };
   isLibrary = true;
   isExecutable = true;
@@ -19,12 +19,12 @@ mkDerivation {
     megaparsec mtl optparse-applicative prettyprinter regex-pcre
     scientific text time
   ];
-  libraryToolDepends = [ hpack ];
   executableHaskellDepends = [ base optparse-applicative ];
   testHaskellDepends = [
-    base tasty tasty-hunit tasty-quickcheck tasty-smallcheck
+    base bytestring filepath megaparsec mtl regex-pcre tasty
+    tasty-golden tasty-hunit tasty-quickcheck tasty-smallcheck text
+    time
   ];
-  preConfigure = "hpack";
   homepage = "https://github.com/sboehler/beans#readme";
   description = "A plain text accounting tool";
   license = stdenv.lib.licenses.bsd3;
