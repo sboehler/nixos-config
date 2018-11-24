a635d42be0591aaf6941569e3d1489ae5e1eea6b
Silvio Böhler <sboehler@noreply.users.github.com>
2018-11-24 14:08:24 +0100

    Allow mDNS in firewall

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,6 +3,14 @@
     enableIPv6 = true;
     firewall = {
       enable = true;
+      allowPing = true;
+      allowedUDPPorts = [
+        5353  # mDNS
+      ];
+      extraCommands = ''
+        # allow multicast
+        iptables -A nixos-fw -d 224.0.0.0/4 -j nixos-fw-accept
+      '';
     };
   };
   boot.kernel.sysctl = {
