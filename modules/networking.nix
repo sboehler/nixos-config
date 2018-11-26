7205be02ef943e98eb2d6ed7265cb0784a89055c
Silvio Böhler <sboehler@noreply.users.github.com>
2018-11-26 13:22:15 +0100

    Wireless & wired concurrently

--- modules/networking.nix
+++ modules/networking.nix
@@ -11,6 +11,10 @@
             MulticastDNS = "yes";
             IPv6PrivacyExtensions = "yes";
           };
+          dhcpConfig = {
+            RouteMetric = 20;
+          };
+
         };
         wired = {
           enable = true;
@@ -20,18 +24,30 @@
             MulticastDNS = "yes";
             IPv6PrivacyExtensions = "yes";
           };
+          dhcpConfig = {
+            RouteMetric = 10;
+          };
         };
       };
     };
   };
 
   networking = {
-    wireless.enable = true;
+    wireless = {
+      enable = true;
+      userControlled = {
+        enable = true;
+      };
+    };
     enableIPv6 = true;
     firewall = {
       enable = true;
       allowPing = true;
+      allowedTCPPorts = [
+        22000
+      ];
       allowedUDPPorts = [
+        21027 # Syncthing discovery
         5353
         5355 # https://en.wikipedia.org/wiki/Link-Local_Multicast_Name_Resolution
       ];
