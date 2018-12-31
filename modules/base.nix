01a679beeef96deebd3c3a3eef22a10debc70d85
Silvio Böhler <sboehler@noreply.users.github.com>
2018-12-31 11:19:32 +0100

    Add user to cdrom

--- modules/base.nix
+++ modules/base.nix
@@ -179,7 +179,7 @@ in
         home = "/home/silvio";
         description = "Silvio Böhler";
         isNormalUser = true;
-        extraGroups = ["wheel" "docker" "libvirtd" "audio" "transmission" "networkmanager"];
+        extraGroups = ["wheel" "docker" "libvirtd" "audio" "transmission" "networkmanager" "cdrom"];
         uid = 1000;
         openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFqXLmL2FVGAkSlndgqaEDx0teA6Ai1wLu21KSdcBnV6XldetAHZ8AAeodgEqIYD/sO69xCm9Kwa3DbktdMO28MO6A7poQ4jvDVHray7mpsm3z5xgc1HAadjNUBvlPjPBbCvZkhcI2/MSvVknl5uFXeH58AqaIq6Ump4gIC27Mj9vLMuw7S5MoR6vJgxKK/h52yuKXs8bisBvrHYngBgxA0wpg/v3G04iplPtTtyIY3uqkgPv3VfMSEyOuZ+TLujFg36FxU5I7Ok0Bjf8f+/OdE41MYYUH1VPIHFtxNs8MPCcz2Sv0baxEhAiEBpnWsQx8mBhxmQ/cK4Ih2EOLqPKR"];
       };
