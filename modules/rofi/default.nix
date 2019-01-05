1d18997e0d934800c7ca3ee46fc0a444c2c7fda3
Silvio Böhler <sboehler@noreply.users.github.com>
2019-01-05 20:57:36 +0100

    Fix rofi script with wayland

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -3,5 +3,6 @@ let
   scripts = ./scripts;
 in
   pkgs.writeShellScriptBin "launch-rofi" ''
-    ls ${scripts} | ${pkgs.rofi}/bin/rofi -dmenu -normal-window | xargs -I TARGET sh ${scripts}/TARGET
+    TERM=dumb
+    ls ${scripts} | ${pkgs.rofi}/bin/rofi -dmenu -normal-window | xargs -I TARGET bash ${scripts}/TARGET
   ''
