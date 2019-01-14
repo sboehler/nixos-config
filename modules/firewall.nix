2a200453be14d22dc4b2d66bd9c8b92456a01bee
Silvio Böhler <sboehler@noreply.users.github.com>
2019-01-14 22:38:54 +0100

    Remove ports which are automatically added by Syncthing

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -3,12 +3,8 @@
     firewall = {
       enable = true;
       allowPing = true;
-      allowedTCPPorts = [
-        22000
-      ];
       allowedUDPPorts = [
-        21027 # Syncthing discovery
-        5353
+        5353 # mDNS
         5355 # https://en.wikipedia.org/wiki/Link-Local_Multicast_Name_Resolution
       ];
     };
