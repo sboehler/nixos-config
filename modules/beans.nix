14b1f8a34a7622731885541d2902c544ad8fbb5d
Silvio Böhler <sboehler@noreply.users.github.com>
2019-02-03 14:06:42 +0100

    Update beans

--- modules/beans.nix
+++ modules/beans.nix
@@ -1,37 +1,37 @@
-{ mkDerivation, base, bifunctors, bytestring, containers, Decimal
+{ mkDerivation, base, bifunctors, bytestring, containers
 , exceptions, fetchgit, filepath, groups, hpack, lens, megaparsec
 , mtl, optparse-applicative, parser-combinators, prettyprinter
-, regex-pcre, scientific, stdenv, tasty, tasty-golden, tasty-hunit
+, regex-pcre, stdenv, tasty, tasty-golden, tasty-hunit
 , tasty-quickcheck, tasty-smallcheck, text, time
 }:
 mkDerivation {
   pname = "beans";
-  version = "0.0.3.0";
+  version = "0.0.4.0";
   src = fetchgit {
     url = "https://github.com/sboehler/beans";
-    sha256 = "0k8cmy9063pi5fniahzbq9phabin3j0njak2y182ap8yhcjlc33y";
-    rev = "6bfa88da0b882a517c661dadbf1548e38135b2c1";
+    sha256 = "1dmkk9qcwacfy192hy1inm1vf4za6g7jr1f1pkay2sfwjicybsny";
+    rev = "0466d13b79ece6ec6ad1859ea8ff0a32d5c16430";
     fetchSubmodules = true;
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base bifunctors bytestring containers Decimal exceptions filepath
-    groups lens megaparsec mtl optparse-applicative parser-combinators
-    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    base bifunctors bytestring containers exceptions filepath groups
+    lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre tasty tasty-golden tasty-hunit
     tasty-quickcheck tasty-smallcheck text time
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base bifunctors bytestring containers Decimal exceptions filepath
-    groups lens megaparsec mtl optparse-applicative parser-combinators
-    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    base bifunctors bytestring containers exceptions filepath groups
+    lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre tasty tasty-golden tasty-hunit
     tasty-quickcheck tasty-smallcheck text time
   ];
   testHaskellDepends = [
-    base bifunctors bytestring containers Decimal exceptions filepath
-    groups lens megaparsec mtl optparse-applicative parser-combinators
-    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    base bifunctors bytestring containers exceptions filepath groups
+    lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre tasty tasty-golden tasty-hunit
     tasty-quickcheck tasty-smallcheck text time
   ];
   preConfigure = "hpack";
