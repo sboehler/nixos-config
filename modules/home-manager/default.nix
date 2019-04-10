6c94b513566d8cfd0a1c90001168b5ed8fddb1a8
Silvio Böhler <sboehler@noreply.users.github.com>
2019-04-10 13:49:50 +0200

    Fix tramp

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -95,6 +95,18 @@
 
         eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
         eval $(${pkgs.coreutils}/bin/dircolors "${./dircolors.ansi-universal}")
+
+        # Fix tramp:
+
+        if [[ "$TERM" == "dumb" ]]
+        then
+          unsetopt zle
+          unsetopt prompt_cr
+          unsetopt prompt_subst
+          unfunction precmd
+          unfunction preexec
+          PS1='$ '
+        fi
       '';
     };
   };
